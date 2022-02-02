7a50abcfdccb926b77177dd7ec56d70d65dacb6e
tattzetey <tattzetey@gmail.com>
2022-02-02 15:17:56 +0000

    remove cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 import bodyParser from 'body-parser';
-import cors from 'cors';
 import express from 'express';
 import { logger } from './middlewares/logger.middleware.js';
 import router from './routers/api.router.js';
 
 const app = express();
-
-app.use(cors());
 
 app.use(express.static('public'));
 
