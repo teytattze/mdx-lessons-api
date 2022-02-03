a45470bacd2d7b267d5552ae928f4c5b3b5a927b
tattzetey <tattzetey@gmail.com>
2022-02-03 15:16:38 +0000

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import { logger } from './middlewares/logger.middleware.js';
 import router from './routers/api.router.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.static('public'));
 
 app.use(bodyParser.json());
